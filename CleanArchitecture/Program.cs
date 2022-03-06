5cadff3c990b3d79968f6b5b5a3eea9794475f2f
mgergis <mnagy@jiosdev.com>
2022-03-06 22:46:35 +0200

    Initial Commits.
    - Creating the project.
    - Adding files to gitignore.
    - Disabling the browserLunch as we will use postman.
    - Configure CORS (default policy) with service extension method.
    - Configure IIS with no options.

---
+++ CleanArchitecture/Program.cs
@@ -0,0 +1,21 @@
+using CleanArchitecture.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.ConfigureCors(); 
+builder.Services.ConfigureIISIntegration();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
